44e4a29ee1f20c3752c46fff4d2035156516ed86
Tushar Adhao <tadhao@gmail.com>
2021-08-24 20:05:40 +0530

    Added get request to fetch data using API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [responseBody, setResponseBody] = useState();
+  
+ const fetchData = () => {
+   fetch('https://swapi.dev/api/starships/22/')
+   .then(response => response.json())
+   .then(data => {
+     console.log(data)
+     setResponseBody(data)
+    })
+    .catch( error => {
+      console.log(error.message)
+    })
+
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Testing HTTP Request</h2>
+      <button onClick={fetchData}>Sync Data</button>
+      <p>Name: {responseBody?.name}</p>
+      <p>Model: {responseBody?.model}</p>
+      <p>Manufactureer: {responseBody?.manufacturer}</p>
     </div>
   );
 }
